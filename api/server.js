c48166ec1bfb050bda1ee552a8a0d57bf55b855c
taufik882 <taufikmahaldi059@gmail.com>
2024-05-29 23:48:45 +0700

    tugas6 commit11

--- api/server.js
+++ api/server.js
@@ -1,9 +1,12 @@
 import jsonServer from 'json-server';
+import cors from 'cors';
+
 const server = jsonServer.create();
 const router = jsonServer.router('db.json');
 const middlewares = jsonServer.defaults();
 
+server.use(cors()); // Tambahkan ini untuk mengatasi masalah CORS
 server.use(middlewares);
-server.use('/api', router);
+server.use(router);
 
 export default server;
