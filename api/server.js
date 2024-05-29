799e46eebc123e02022d73d4966ae924ac9d298f
taufik882 <taufikmahaldi059@gmail.com>
2024-05-29 22:01:19 +0700

    tugas prak pbk prt6

--- api/server.js
+++ api/server.js
@@ -6,8 +6,4 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use('/api', router);
 
-server.listen(3000, () => {
-  console.log('JSON Server is running');
-});
-
 export default server;
--- vite.config.js
+++ vite.config.js
@@ -1,16 +1,9 @@
-import { defineConfig } from 'vite'
-import vue from '@vitejs/plugin-vue'
+import { defineConfig } from 'vite';
+import vue from '@vitejs/plugin-vue';
 
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'dist',
-    rollupOptions: {
-      output: {
-        entryFileNames: 'assets/[name].js',
-        chunkFileNames: 'assets/[name].js',
-        assetFileNames: 'assets/[name].[ext]'
-      }
-    }
+    outDir: 'dist'
   }
-})
+});
