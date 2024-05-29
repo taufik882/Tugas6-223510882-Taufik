6aad3e3706b2b395117040f030d6cf7dad0c6a98
taufik882 <taufikmahaldi059@gmail.com>
2024-05-29 21:50:29 +0700

    tugas prak pbk pert6

--- api/server.js
+++ api/server.js
@@ -6,4 +6,8 @@ const middlewares = jsonServer.defaults();
 server.use(middlewares);
 server.use('/api', router);
 
+server.listen(3000, () => {
+  console.log('JSON Server is running');
+});
+
 export default server;
