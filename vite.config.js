75f60f7ea58f99ee4a76588f049f659ca7857d1c
taufik882 <taufikmahaldi059@gmail.com>
2024-05-29 20:19:58 +0700

    prak pbk p6

--- vite.config.js
+++ vite.config.js
@@ -4,6 +4,13 @@ import vue from '@vitejs/plugin-vue'
 export default defineConfig({
   plugins: [vue()],
   build: {
-    outDir: 'dist'
+    outDir: 'dist',
+    rollupOptions: {
+      output: {
+        entryFileNames: 'assets/[name].js',
+        chunkFileNames: 'assets/[name].js',
+        assetFileNames: 'assets/[name].[ext]'
+      }
+    }
   }
 })
